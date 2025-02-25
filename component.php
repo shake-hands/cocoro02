be4667721817ec141055f2a20116b8f22ad28e5a
就労支援デザイン事務所Shake Hands <104177532+shake-hands@users.noreply.github.com>
2025-02-25 15:59:58 +0900

    Add files via upload

---
+++ component.php
@@ -0,0 +1,36 @@
+<?php
+// component.php 應包含正確的 $product 陣列定義
+$product = [
+    [
+        "product_name" => "徳利1_白化粧に土灰釉",
+        "price" => 3000,
+        "img" => "photo/to1.jpg", // 相對於 htdocs 的路徑
+    ],
+    [
+        "product_name" => "徳利2_織部釉",
+        "price" => 3500,
+        "img" => "photo/to2.jpg",
+    ],
+    [
+        "product_name" => "徳利3_織部釉",
+        "price" => 4000,
+        "img" => "photo/to3.jpg",
+    ],
+    [
+        "product_name" => "ぐい飲み1_白萩釉",
+        "price" => 2000,
+        "img" => "photo/gu1.jpg",
+    ],[
+        "product_name" => "ぐい飲み2_白化粧に白萩釉",
+        "price" => 2500,
+        "img" => "photo/gu2.jpg",
+    ],
+    [
+        "product_name" => "長皿1_石灰白萩釉",
+        "price" => 6000,
+        "img" => "photo/pl1.jpg",
+    ],
+    
+
+];
+?>
